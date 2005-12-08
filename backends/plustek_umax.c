882d65cb8cc0c3221fb4b392bccc51692c6d1416
root84 <>
2005-12-08 14:38:57 +0000

    fixed some comments

--- backends/plustek_umax.c
+++ backends/plustek_umax.c
@@ -215,28 +215,6 @@ int plustek_write(scanner_t* scanner, void* buffer, int bytecount)
 
 int scanbtnd_get_button(scanner_t* scanner)
 {
-	/*
-	Note 1: I strongly suspect that the command 0x01 0x69 0x00 0x01 will return
-	a button bitmask. For my Canon N1220U it returns 0x04, which happens to
-	be the bit I have to test against to see if the scanner button was pressed.
-	However, this has to be tested on other scanners to see if this is true.
-	UPDATE by BS: The LIDE 20 also returns 0x04, but it has three buttons!
-	So this guess is probably wrong. (Thanks to Christian Bucher for this info)
-	
-	Note 2: This works on my Canon N1220U. Whether this is Canon specific or
-	if it works for all 'plustek usb' type scanners is something I don't know.
-
-	Note 3: You must have run sane-find-scanner once. Sane apparently initializes
-	something on the scanner allowing this to work. Otherwise all you get is 0x00.
-	
-	Note 4: by /cbx
-	On my CanoScan LIDE20, the default value is $62 and the bits for the
-	buttons are as follows:
-	Scan: $72 ==> 0x10
-	Copy: $6a ==> 0x08
-	Mail: $66 ==> 0x04
-	*/
-
 	unsigned char bytes[255];
 	int num_bytes;
 	int button = 0;
@@ -251,25 +229,20 @@ int scanbtnd_get_button(scanner_t* scanner)
 	num_bytes = plustek_read(scanner, (void*)bytes, 1);
 	if (num_bytes != 1) return 0;
 	
-	// by BS: This is my first attempt to get rid of the 
-	// hardcoded button bitmask. Note that I do not own any device
-	// supported by this backend, so this code is based on guessing.
-	// Tested on the LIDE 20, should work for 1-button devices, too.
 	switch (scanner->num_buttons) {
-	case 1:
+	case 1: // not tested
 		if ((bytes[0] & 0x04) != 0) button = 1;
 		break;
-	case 2:
+	case 2: // not tested
 		if ((bytes[0] & 0x08) != 0) button = 1;
 		if ((bytes[0] & 0x04) != 0) button = 2;
 		break;
-	case 3: 
+	case 3: // not tested
 		if ((bytes[0] & 0x10) != 0) button = 1;
 		if ((bytes[0] & 0x08) != 0) button = 2;
 		if ((bytes[0] & 0x04) != 0) button = 3;
 		break;	
-	case 4: // only tested for the Epson Perfection 1260...
-		// seems to be a bit odd compared to the other cases...
+	case 4: // only tested on UMAX Astra 3400
 		if ((bytes[0] & 0x04) != 0) button = 1;
 		if ((bytes[0] & 0x08) != 0) button = 2;
 		if ((bytes[0] & 0x40) != 0) button = 3;
