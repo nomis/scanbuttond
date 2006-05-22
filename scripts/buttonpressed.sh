342074bef56cbb81c8de5c7c7792eef82fe349ee
root84 <>
2006-05-22 02:47:22 +0000

    Added artec_eplus48u backend

--- scripts/buttonpressed.sh
+++ scripts/buttonpressed.sh
@@ -18,17 +18,33 @@ case $1 in
 		# Fine-tuned for the Epson Perfection 2400, the HP LaserJet 1200 and
 		# ISO A4 paper size so that the scanned document matches the printer
 		# output as closely as possible.
+		# The festival speech synthesizer is used to inform the user about
+		# the progress of the operation.
+		#
 		#
 		# if [ -f $LOCKFILE ]; then
-		#   echo "Error: Another scanning operation is currently in progress"
+		#   echo "Error: Another scanning operation is currently in progress" | festival --tts
 		#   exit
 		# fi
 		# touch $LOCKFILE
 		# rm -f $TMPFILE
+		# echo "Copying" | festival --tts
 		# scanimage --device-name $2 --format tiff --mode Gray --quick-format A4 \
 		# --resolution 300 --sharpness 0 --brightness -3 \
 		# --gamma-correction "High contrast printing" > $TMPFILE
+		# if [ $? != 0 ]; then
+		# 	echo "Scanning failed" | festival --tts
+		# 	rm $LOCKFILE
+		# 	exit
+		# fi
+		# echo "Submitting print job" | festival --tts
 		# tiff2ps -z -w 8.27 -h 11.69 $TMPFILE | lpr
+		# if [ $? != 0 ]; then
+		# 	echo "Printing failed" | festival --tts
+		# 	rm $LOCKFILE
+		# 	exit
+		# fi
+		# echo "The print job has been submitted" | festival --tts
 		# rm -f $LOCKFILE
 		#
 		;;
